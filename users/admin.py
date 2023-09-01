52f0ca01239f5cb3c05cb91c478eebbdc9a4dffa
Joy Ahmed <dev.joyahmed@gmail.com>
2023-09-01 08:04:17 +0600

    user model and admin dash with models.user🧑🏻‍💻

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+
+
+@admin.register(models.User)
+class CustomAdmin(admin.ModelAdmin):
+    """ Custom User Admin """
+    list_display = ('username', 'gender', 'language', 'currency', 'superhost')
+    list_filter = ('currency', 'language', 'superhost',)
--- users/models.py
+++ users/models.py
@@ -5,4 +5,45 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    """ Custom user Model """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHERS = "others"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, 'Male'),
+        (GENDER_FEMALE, 'Female'),
+        (GENDER_OTHERS, 'Others')
+    )
+
+    LANGUAGE_ENGLISH = 'en'
+    LANGUAGE_BANGLA = 'bn'
+    LANGUAGE_SPANISH = 'sp'
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, 'English'),
+        (LANGUAGE_BANGLA, 'Bangla'),
+        (LANGUAGE_SPANISH, 'Spanish'),
+    )
+
+    CURRENCY_USD = 'usd'
+    CURRENCY_BDT = 'bdt'
+    CURRENCY_EURO = 'eur'
+
+    CURRENCY_CHOICES = (
+        (CURRENCY_USD, '$ USD'),
+        (CURRENCY_BDT, '৳ BDT'),
+        (CURRENCY_EURO, '€ EUR'),
+    )
+
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES,
+                              max_length=10, null=True, blank=True)
+    bio = models.TextField(default="", blank=True)
+    birthdate = models.DateField(null=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=2, null=True, blank=True)
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=3, null=True, blank=True)
+    superhost = models.BooleanField(default=False)
