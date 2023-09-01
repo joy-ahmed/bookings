11bf659d959b9a4e31fca281f03dd287f99a629d
Joy Ahmed <dev.joyahmed@gmail.com>
2023-09-01 08:14:32 +0600

    extended user profile wirh django itself⬆️

--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 # Register your models here.
 
 
 @admin.register(models.User)
-class CustomAdmin(admin.ModelAdmin):
+class CustomAdmin(UserAdmin):
     """ Custom User Admin """
-    list_display = ('username', 'gender', 'language', 'currency', 'superhost')
-    list_filter = ('currency', 'language', 'superhost',)
+    # list_display = ('username', 'gender', 'language', 'currency', 'superhost')
+    # list_filter = ('currency', 'language', 'superhost',)
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom profile", {
+                "fields": ("avatar", "gender", "bio", "birthdate", "language", "superhost")
+            }
+        ),
+    )
